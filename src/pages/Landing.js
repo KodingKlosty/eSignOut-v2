2cf14bb4e2ff45ddc713363b6793617d7e880f1b
James Klostermeyer Jr <42751399+KodingKlosty@users.noreply.github.com>
2020-07-06 13:15:39 -0400

    added SASS; Updated Landing.js
    Landing page updated

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,13 +1,20 @@
 import React, {Component} from 'react'
 
 
-
-
 class Landing extends Component {
     render(){
         return(
             <div className="text-center">
-                <h2>This is the Landing Page</h2>
+                <h1 className="h2">ESignout Version 2.0</h1>
+                <h2 className="h3">Upgrades are still being implimented </h2>
+                <div className="form-reg">
+                    <p >Please feel free to look look around. The site is still under revamp but will 
+                        be operational very soon. This website was build with MERN Full Stack mindset
+                        and is using Bootstrap 5 alpha 1 and Sass for UI/UX components. If you would like
+                        to view the GITHUB for this site or make your own fork to play with, please 
+                        <a href="https://github.com/KodingKlosty/eSignOut-v2"> Click Here</a>
+                    </p>
+                </div>
             </div>
         )
     }
