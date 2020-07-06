a813741fe90956628161b4cc12ecf53091b201fe
James Klostermeyer Jr <42751399+KodingKlosty@users.noreply.github.com>
2020-07-06 16:58:14 -0400

    folder restructure
    express moved to root
    react moved to client

--- server.js
+++ server.js
@@ -30,9 +30,9 @@ app.use("/locations", require('./routes/locations'))
 
 //Serve Static assets if in production
 if(process.env.NODE_ENV === "production"){
-    app.use(express.static('api/build'));
+    app.use(express.static('client/build'));
     app.get('*',(req, res) => {
-        res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     })
 }
 
