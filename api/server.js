56fe3635a8276eb78ef5878c7bc62aa26f8b7ddf
James Klostermeyer Jr <42751399+KodingKlosty@users.noreply.github.com>
2020-07-06 16:09:10 -0400

    Update server.js

--- api/server.js
+++ api/server.js
@@ -32,7 +32,7 @@ app.use("/locations", require('./routes/locations'))
 if(process.env.NODE_ENV === "production"){
     app.use(express.static('client/build'));
     app.get('*',(req, res) => {
-        res.sendFile(path.resolve(__dirname, 'src', 'build', 'index.html'));
+        res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
     })
 }
 
